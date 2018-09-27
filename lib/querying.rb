4b1886dde59e183369515de224ef2540c3f9388a
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-27 18:55:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year 
-  FROM books WHERE series_id = 1 
+  FROM books 
+  WHERE series_id = 1 
   ORDER BY books.year;"
 end
 
@@ -30,7 +31,12 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT books.title FROM books JOIN character_books ON books.id = character_books.book_id JOIN characters ON character_books.character_id = characters.id "
+  "SELECT books.title 
+  FROM books 
+  JOIN character_books 
+  ON books.id = character_books.book_id 
+  JOIN characters 
+  ON character_books.character_id = characters.id "
 end
 
 def select_character_names_and_number_of_books_they_are_in
