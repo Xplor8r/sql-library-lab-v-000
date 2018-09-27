4fbbbda717df2f78345013c462669ab9bae6d36d
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-27 19:05:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,9 @@ def select_series_title_with_most_human_characters
   JOIN books 
   ON series.id = books.series_id
   JOIN character_books
-  ON character_books.book_id = bo
+  ON character_books.book_id = books.id
+  JOIN characters
+  ON character_books.CH
   DESC LIMIT 1;"
 end
 
